5753da8e756273572a9fac136876380f0336406d
hunghuynh9822 <31771988+hunghuynh9822@users.noreply.github.com>
2019-09-17 00:55:00 +0700

    sticky header

--- src/client/src/components/Tables/PaginationTable.jsx
+++ src/client/src/components/Tables/PaginationTable.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 
 import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableFooter from '@material-ui/core/TableFooter';
@@ -20,29 +21,84 @@ const styles = theme => ({
         minWidth: 500,
     },
     tableWrapper: {
+        maxHeight: 407,
         overflowX: 'auto',
     },
+    stickyHeader: {
+        backgroundColor: "#fff",
+        position: "sticky",
+        top: 0,
+    }
 });
 
-function createData(name, calories, fat) {
-    return { name, calories, fat };
+// function createData(name, calories, fat) {
+//     return { name, calories, fat };
+// }
+
+// const rows = [
+//     createData('Cupcake', 305, 3.7),
+//     createData('Donut', 452, 25.0),
+//     createData('Eclair', 262, 16.0),
+//     createData('Frozen yoghurt', 159, 6.0),
+//     createData('Gingerbread', 356, 16.0),
+//     createData('Honeycomb', 408, 3.2),
+//     createData('Ice cream sandwich', 237, 9.0),
+//     createData('Jelly Bean', 375, 0.0),
+//     createData('KitKat', 518, 26.0),
+//     createData('Lollipop', 392, 0.2),
+//     createData('Marshmallow', 318, 0),
+//     createData('Nougat', 360, 19.0),
+//     createData('Oreo', 437, 18.0),
+// ].sort((a, b) => (a.calories < b.calories ? -1 : 1));
+
+const columns = [
+    { id: 'name', label: 'Name', minWidth: 200 },
+    { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+    {
+        id: 'population',
+        label: 'Population',
+        minWidth: 120,
+        align: 'right',
+        format: value => value.toLocaleString(),
+    },
+    {
+        id: 'size',
+        label: 'Size\u00a0(km\u00b2)',
+        minWidth: 120,
+        align: 'right',
+        format: value => value.toLocaleString(),
+    },
+    {
+        id: 'density',
+        label: 'Density',
+        minWidth: 120,
+        align: 'right',
+        format: value => value.toFixed(2),
+    },
+];
+
+function createData(name, code, population, size) {
+    const density = population / size;
+    return { name, code, population, size, density };
 }
 
 const rows = [
-    createData('Cupcake', 305, 3.7),
-    createData('Donut', 452, 25.0),
-    createData('Eclair', 262, 16.0),
-    createData('Frozen yoghurt', 159, 6.0),
-    createData('Gingerbread', 356, 16.0),
-    createData('Honeycomb', 408, 3.2),
-    createData('Ice cream sandwich', 237, 9.0),
-    createData('Jelly Bean', 375, 0.0),
-    createData('KitKat', 518, 26.0),
-    createData('Lollipop', 392, 0.2),
-    createData('Marshmallow', 318, 0),
-    createData('Nougat', 360, 19.0),
-    createData('Oreo', 437, 18.0),
-].sort((a, b) => (a.calories < b.calories ? -1 : 1));
+    createData('India', 'IN', 1324171354, 3287263),
+    createData('China', 'CN', 1403500365, 9596961),
+    createData('Italy', 'IT', 60483973, 301340),
+    createData('United States', 'US', 327167434, 9833520),
+    createData('Canada', 'CA', 37602103, 9984670),
+    createData('Australia', 'AU', 25475400, 7692024),
+    createData('Germany', 'DE', 83019200, 357578),
+    createData('Ireland', 'IE', 4857000, 70273),
+    createData('Mexico', 'MX', 126577691, 1972550),
+    createData('Japan', 'JP', 126317000, 377973),
+    createData('France', 'FR', 67022000, 640679),
+    createData('United Kingdom', 'GB', 67545757, 242495),
+    createData('Russia', 'RU', 146793744, 17098246),
+    createData('Nigeria', 'NG', 200962417, 923768),
+    createData('Brazil', 'BR', 210147125, 8515767),
+];
 
 class PaginationTable extends Component {
     constructor(props) {
@@ -68,48 +124,69 @@ class PaginationTable extends Component {
     }
     render() {
         const { classes } = this.props;
-        const emptyRows = this.state.rowsPerPage - Math.min(this.state.rowsPerPage, rows.length - this.state.page * this.state.rowsPerPage);
+        const { page, rowsPerPage } = this.state;
+        const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
         return (
             <Paper className={classes.root}>
                 <div className={classes.tableWrapper}>
-                    <Table className={classes.table}>
-                        <TableBody>
-                            {rows.slice(this.state.page * this.state.rowsPerPage, this.state.page * this.state.rowsPerPage + this.state.rowsPerPage).map(row => (
-                                <TableRow key={row.name}>
-                                    <TableCell component="th" scope="row">
-                                        {row.name}
+                    <Table >
+                        <TableHead>
+                            <TableRow>
+                                {columns.map(column => (
+                                    <TableCell
+                                        key={column.id}
+                                        align={column.align}
+                                        style={{ minWidth: column.minWidth }}
+                                        className={classes.stickyHeader}
+                                    >
+                                        {column.label}
                                     </TableCell>
-                                    <TableCell align="right">{row.calories}</TableCell>
-                                    <TableCell align="right">{row.fat}</TableCell>
+                                ))}
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => (
+                                // <TableRow key={row.name}>
+                                //     <TableCell component="th" scope="row">
+                                //         {row.name}
+                                //     </TableCell>
+                                //     <TableCell align="right">{row.calories}</TableCell>
+                                //     <TableCell align="right">{row.fat}</TableCell>
+                                // </TableRow>
+                                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                                    {columns.map(column => {
+                                        const value = row[column.id];
+                                        return (
+                                            <TableCell key={column.id} align={column.align}>
+                                                {column.format && typeof value === 'number' ? column.format(value) : value}
+                                            </TableCell>
+                                        );
+                                    })}
                                 </TableRow>
                             ))}
-
-                            {emptyRows > 0 && (
+                            {/* {emptyRows > 0 && (
                                 <TableRow style={{ height: 48 * emptyRows }}>
                                     <TableCell colSpan={6} />
                                 </TableRow>
-                            )}
+                            )} */}
                         </TableBody>
-                        <TableFooter>
-                            <TableRow>
-                                <TablePagination
-                                    rowsPerPageOptions={[5, 10, 25]}
-                                    colSpan={3}
-                                    count={rows.length}
-                                    rowsPerPage={this.state.rowsPerPage}
-                                    page={this.state.page}
-                                    SelectProps={{
-                                        inputProps: { 'aria-label': 'rows per page' },
-                                        native: true,
-                                    }}
-                                    onChangePage={this.handleChangePage}
-                                    onChangeRowsPerPage={this.handleChangeRowsPerPage}
-                                    ActionsComponent={Pagination}
-                                />
-                            </TableRow>
-                        </TableFooter>
                     </Table>
                 </div>
+                <TablePagination
+                    rowsPerPageOptions={[5, 10, 25]}
+                    component="div"
+                    colSpan={3}
+                    count={rows.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    SelectProps={{
+                        inputProps: { 'aria-label': 'rows per page' },
+                        native: true,
+                    }}
+                    onChangePage={this.handleChangePage}
+                    onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                    ActionsComponent={Pagination}
+                />
             </Paper>
         );
     }
