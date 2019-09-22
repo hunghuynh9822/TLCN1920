62049cbfbe1883f42032da62ecfc31ea6caa0c4f
hunghuynh9822 <31771988+hunghuynh9822@users.noreply.github.com>
2019-09-22 15:23:07 +0700

    add card

--- src/client/src/components/Task/Task.jsx
+++ src/client/src/components/Task/Task.jsx
@@ -1,57 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import clsx from 'clsx';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import GridListTile from '@material-ui/core/GridListTile';
-const styles = theme => ({
-    grid: {
-        height: 'auto',
-        flexDirection: 'column',
-        position: 'relative',
-        display: 'flex',
-        marginLeft: '20px',
-        marginRight: '20px',
-        marginBottom: '10px',
-    },
-    card: {
-        width: 'auto',
-    },
-    card_header: {
-        backgroundColor: theme.color.background.blue,
-        height: 60,
-    },
-    media: {
-        height: 0,
-        paddingTop: '56.25%', // 16:9
-    },
-    expand: {
-        transform: 'rotate(0deg)',
-        marginLeft: 'auto',
-        transition: theme.transitions.create('transform', {
-            duration: theme.transitions.duration.shortest,
-        }),
-    },
-    expandOpen: {
-        transform: 'rotate(180deg)',
-    },
-    avatar: {
-        backgroundColor: red[500],
-    },
+
+const styles = theme => ({ 
+    "checked": { "color": "orange" }, 
+    "fa": { "fontSize": "10px", "width": "5px" },
+    "root":{
+        "padding":"10px"
+    }
 });
 class Task extends Component {
     constructor(props) {
@@ -62,87 +18,78 @@ class Task extends Component {
     }
     render() {
         const { classes } = this.props;
-        const handleExpandClick = () => {
-            this.setState({
-                expanded: !this.state.expanded,
-            })
-        }
         return (
-            <Grid item className={classes.grid}>
-                <Card className={classes.card}>
-                    <CardHeader
-                        avatar={
-                            <Avatar aria-label="recipe" className={classes.avatar}>
-                                R
-                            </Avatar>
-                        }
-                        action={
-                            <IconButton aria-label="settings">
-                                <MoreVertIcon />
-                            </IconButton>
-                        }
-                        title="Shrimp and Chorizo Paella"
-                        subheader="September 14, 2016"
-                        className={classes.card_header}
-                    />
-                    <CardMedia
-                        className={classes.media}
-                        // image="/static/images/cards/paella.jpg"
-                        title="Paella dish"
-                    />
-                    <CardContent>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            This impressive paella is a perfect party dish and a fun meal to cook together with your
-                            guests. Add 1 cup of frozen peas along with the mussels, if you like.
-        </Typography>
-                    </CardContent>
-                    <CardActions disableSpacing>
-                        <IconButton aria-label="add to favorites">
-                            <FavoriteIcon />
-                        </IconButton>
-                        <IconButton aria-label="share">
-                            <ShareIcon />
-                        </IconButton>
-                        <IconButton
-                            className={clsx(classes.expand, {
-                                [classes.expandOpen]: this.state.expanded,
-                            })}
-                            onClick={handleExpandClick}
-                            aria-expanded={this.state.expanded}
-                            aria-label="show more"
-                        >
-                            <ExpandMoreIcon />
-                        </IconButton>
-                    </CardActions>
-                    <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
-                        <CardContent>
-                            <Typography paragraph>Method:</Typography>
-                            <Typography paragraph>
-                                Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-                                minutes.
-          </Typography>
-                            <Typography paragraph>
-                                Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-                                heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-                                browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-                                and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-                                pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-                                saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-                            <Typography paragraph>
-                                Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-                                without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-                                medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-                                again without stirring, until mussels have opened and rice is just tender, 5 to 7
-                                minutes more. (Discard any mussels that don’t open.)
-          </Typography>
-                            <Typography>
-                                Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>
-                        </CardContent>
-                    </Collapse>
-                </Card>
-            </Grid >
+            <div className={classes.root}>
+                <ul className="list-group">
+                    <li className="list-group-item active">
+                        <div className="row">
+                            <div className="col-10"><a>Thái Thanh Liêm</a></div>
+                            <div className="col-2"><i className="fas fa-plus" style={{ float: 'right' }} /></div>
+                        </div>
+                    </li>
+                    <li className="list-group-item">
+                        <div className="row">
+                            <div className="col-8"><a>1/3 Task With Point</a></div>
+                            <div className="col-4"><a style={{ float: 'right' }}>5 Point</a></div>
+                        </div>
+                    </li>
+                    <li className="list-group-item">
+                        <div className="row">
+                            <div className="col-8">
+                                <div className="form-check">
+                                    <label className="form-check-label" htmlFor="check1">
+                                        <input type="checkbox" className="form-check-input" id="check1" name="option1" defaultValue="something" />Task 1
+          </label>
+                                </div>
+                            </div>
+                            <div className="col-4" style={{ float: 'right' }}>
+                                <span className="fa fa-star checked" />
+                                <span className="fa fa-star checked" />
+                                <span className="fa fa-star checked" />
+                                <span className="fa fa-star" />
+                                <span className="fa fa-star" />
+                            </div>
+                        </div>
+                    </li>
+                    <li className="list-group-item">
+                        <div className="row">
+                            <div className="col-8">
+                                <div className="form-check">
+                                    <label className="form-check-label" htmlFor="check1">
+                                        <input type="checkbox" className="form-check-input" id="check1" name="option1" defaultValue="something" />Task 2
+          </label>
+                                </div>
+                            </div>
+                            <div className="col-4" style={{ float: 'right' }}>
+                                <span className="fa fa-star checked" />
+                                <span className="fa fa-star checked" />
+                                <span className="fa fa-star checked" />
+                                <span className="fa fa-star" />
+                                <span className="fa fa-star" />
+                            </div>
+                        </div>
+                    </li>
+                    <li className="list-group-item "><div className="row" style={{ height: '6px' }}><a style={{ fontSize: '12px' }}>Completed Task</a></div></li>
+                    <li className="list-group-item">
+                        <div className="row">
+                            <div className="col-8">
+                                <div className="form-check">
+                                    <label className="form-check-label" htmlFor="check1">
+                                        <input type="checkbox" className="form-check-input" id="check1" name="option1" defaultValue="something" defaultChecked />Task 3
+          </label>
+                                </div>
+                            </div>
+                            <div className="col-4" style={{ float: 'right' }}>
+                                <span className="fa fa-star checked" />
+                                <span className="fa fa-star checked" />
+                                <span className="fa fa-star checked" />
+                                <span className="fa fa-star" />
+                                <span className="fa fa-star" />
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         );
     }
 }
