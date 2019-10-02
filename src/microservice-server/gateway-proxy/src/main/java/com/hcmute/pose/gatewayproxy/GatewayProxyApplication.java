2073bc84e2c4094db0615502aabcda602b9eb7ce
hunghuynh9822 <31771988+hunghuynh9822@users.noreply.github.com>
2019-10-03 01:24:43 +0700

    fix gateway proxy

--- src/microservice-server/gateway-proxy/src/main/java/com/hcmute/pose/gatewayproxy/GatewayProxyApplication.java
+++ src/microservice-server/gateway-proxy/src/main/java/com/hcmute/pose/gatewayproxy/GatewayProxyApplication.java
@@ -1,13 +1,37 @@
 package com.hcmute.pose.gatewayproxy;
 
+import com.hcmute.pose.gatewayproxy.filters.ErrorFilter;
+import com.hcmute.pose.gatewayproxy.filters.PostFilter;
+import com.hcmute.pose.gatewayproxy.filters.PreFilter;
+import com.hcmute.pose.gatewayproxy.filters.RouteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
 public class GatewayProxyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayProxyApplication.class, args);
 	}
-
+    @Bean
+    public PreFilter preFilter() {
+        return new PreFilter();
+    }
+    @Bean
+    public PostFilter postFilter() {
+        return new PostFilter();
+    }
+    @Bean
+    public ErrorFilter errorFilter() {
+        return new ErrorFilter();
+    }
+    @Bean
+    public RouteFilter routeFilter() {
+        return new RouteFilter();
+    }
 }
--- src/microservice-server/task-service/src/main/java/com/hcmute/pose/taskservice/config/TaskConfig.java
+++ src/microservice-server/task-service/src/main/java/com/hcmute/pose/taskservice/config/TaskConfig.java
@@ -1,12 +1,21 @@
 package com.hcmute.pose.taskservice.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class TaskConfig {
     @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    @Bean
+    @LoadBalanced
     public WebClient.Builder builder(){
         return WebClient.builder();
     }
