d306c9dd97d255bcab252e2e3893a2db14188fbb
hunghuynh9822 <31771988+hunghuynh9822@users.noreply.github.com>
2020-01-04 20:57:19 +0700

    export server url

--- src/client/src/action/index.js
+++ src/client/src/action/index.js
@@ -3,7 +3,7 @@ import {
     ACCESS_TOKEN
 } from '../constants';
 
-const serverUrl = process.env.SERVER_HOST || 'http://192.168.200.1:8080';
+export const serverUrl = process.env.SERVER_HOST || 'http://192.168.200.1:8080';
 const api = axios.create({
     baseURL: serverUrl
 });
