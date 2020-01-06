fef7a64c436168d34c38c4ae9a8dc3f8259fb927
liemit98 <thaithanhliem2704@gmail.com>
2020-01-06 14:49:01 +0700

    fix localhost

--- src/client/src/action/index.js
+++ src/client/src/action/index.js
@@ -3,7 +3,8 @@ import {
     ACCESS_TOKEN
 } from '../constants';
 
-export const serverUrl = process.env.SERVER_HOST || 'http://192.168.200.1:8080';
+// export const serverUrl = process.env.SERVER_HOST || 'http://192.168.200.1:8080';
+export const serverUrl = 'http://localhost:8080';
 const api = axios.create({
     baseURL: serverUrl
 });
