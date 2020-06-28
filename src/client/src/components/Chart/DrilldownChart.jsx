b9e8aedc75fa83a0aca792af5827cbfb3744db06
Huynh Le Huu Hung <hunghlh@Macintoshs-MacBook-Pro-3.local>
2020-06-28 12:38:45 +0700

    clean code

--- src/client/src/components/Chart/DrilldownChart.jsx
+++ src/client/src/components/Chart/DrilldownChart.jsx
@@ -88,62 +88,6 @@ class DrilldownChart extends Component {
 	}
 
 	render() {
-		this.options = {
-			"New vs Returning Visitors": [{
-				click: this.onClickChartDrilldownHandler,
-				cursor: "pointer",
-				explodeOnClick: false,
-				innerRadius: "75%",
-				legendMarkerType: "square",
-				name: "New vs Returning Visitors",
-				radius: "100%",
-				showInLegend: true,
-				startAngle: 90,
-				type: "doughnut",
-				dataPoints: [
-					{ y: 522460, name: "New Visitors", color: "#E7823A" },
-					{ y: 307040, name: "Returning Visitors", color: "#546BC1" }
-				]
-			}],
-			"New Visitors": [{
-				color: "#E7823A",
-				name: "New Visitors",
-				type: "column",
-				dataPoints: [
-					{ x: new Date("1 Jan 2017"), y: 37000 },
-					{ x: new Date("1 Feb 2017"), y: 39960 },
-					{ x: new Date("1 Mar 2017"), y: 41160 },
-					{ x: new Date("1 Apr 2017"), y: 42240 },
-					{ x: new Date("1 May 2017"), y: 42200 },
-					{ x: new Date("1 Jun 2017"), y: 43600 },
-					{ x: new Date("1 Jul 2017"), y: 45560 },
-					{ x: new Date("1 Aug 2017"), y: 47280 },
-					{ x: new Date("1 Sep 2017"), y: 48800 },
-					{ x: new Date("1 Oct 2017"), y: 52720 },
-					{ x: new Date("1 Nov 2017"), y: 56840 },
-					{ x: new Date("1 Dec 2017"), y: 58400 }
-				]
-			}],
-			"Returning Visitors": [{
-				color: "#546BC1",
-				name: "Returning Visitors",
-				type: "column",
-				dataPoints: [
-					{ x: new Date("1 Jan 2017"), y: 19000 },
-					{ x: new Date("1 Feb 2017"), y: 21040 },
-					{ x: new Date("1 Mar 2017"), y: 21840 },
-					{ x: new Date("1 Apr 2017"), y: 22760 },
-					{ x: new Date("1 May 2017"), y: 24800 },
-					{ x: new Date("1 Jun 2017"), y: 24400 },
-					{ x: new Date("1 Jul 2017"), y: 25440 },
-					{ x: new Date("1 Aug 2017"), y: 27720 },
-					{ x: new Date("1 Sep 2017"), y: 27200 },
-					{ x: new Date("1 Oct 2017"), y: 29280 },
-					{ x: new Date("1 Nov 2017"), y: 31160 },
-					{ x: new Date("1 Dec 2017"), y: 32400 }
-				]
-			}]
-		}
 		const buttonStyle = {
 			bordeRadius: '4px',
 			padding: '8px',
